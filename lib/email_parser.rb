dedd44f891b3a96f70dc0155960b35a2df849bfd
Alberto Morales <cmtmorales24@gmail.com>
2019-03-24 21:16:18 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,11 +9,11 @@ class EmailParser
   
 
 def initialize(emails)
-binding.pry
+
 end
 
 def parse
-  
+    
     EmailParser.new(emails)
     parser=EmailParser.new(emails)#.delete(",").split.uniq
     
